f3e919d07270ecde0ab4852642ccea1b29317f2c
samanthafernandez <sf@jin.fr>
2016-07-25 11:59:05 +0200

    news

---
+++ database/migrations/2016_07_25_085758_create_choices_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('choices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('choices');
+    }
+}
